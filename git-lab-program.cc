9f0c0d90459b74bd803ddf29387dd5b178cafe82
AlexHolly978 <ah592519@ohio/edu>
2021-09-01 17:00:27 -0400

    inital commit

---
+++ git-lab-program.cc
@@ -0,0 +1,14 @@
+/* 
+file git-lab-program.cc
+author: Alex Holly
+Date: 9/1/2001
+first file
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib> 
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello git!" << endl;
+}
